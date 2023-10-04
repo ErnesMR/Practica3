3656755a89ab9339e31d0be738d361f69bb44b0c
Morales Ramos <emramos@minsait.com>
2023-10-03 19:20:34 -0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,35 @@ import java.util.function.*;
 
 public class Main {
     public static void main(String[] args) {
-        Persona alumno = new Persona();
-        alumno.setNombre("Ernesto");
-        alumno.setApellido("Morales");
-        alumno.setEdad(26);
+        Persona alumno1 = new Persona();
+        alumno1.setNombre("Ernesto");
+        alumno1.setApellido("Morales");
+        alumno1.setEdad(26);
 
 
         Predicate<Persona>esMayor= s ->  s.getEdad() <=18;
-        Supplier<Integer>anoActual= ()-> 2023;
+        System.out.println(esMayor.test(alumno1));
+
+        Supplier<Integer>edadAlumno= ()-> alumno1.getEdad();
+        System.out.println(edadAlumno.get());
+
         Consumer<Persona>nombre=(nom)-> System.out.println(nom.getNombre());
-        //Function<Persona,Integer>edadAlumno=(ed)-> ed.getEdad()*2;
-        Function<Persona,Integer>edadAlumno=Persona::getEdad;
+        nombre.accept(alumno1);
+
+        Function<Persona,String>apellidoAlumno=Persona::getApellido;
+        System.out.println(apellidoAlumno.apply(alumno1));
+
 
        BiFunction<Integer,Integer,Integer>promedioCalificaciones =(cal1,cal2)-> (cal1+cal2) /2;
+       System.out.println(promedioCalificaciones.apply(8,9));
+
        BiPredicate<Integer,Integer>igualandoCalificaciones=(calMate,calEspanol)-> calMate.equals(calEspanol);
-       BiConsumer<String,String>nombrecompleto=(nombreAlumno,apellido)-> System.out.println(nombreAlumno +" "+apellido);
+       System.out.println(  igualandoCalificaciones.test(8,9));
+
+       BiConsumer<Persona,Persona>nombrecompleto=(nombreAlumno,apellido)-> System.out.println(nombreAlumno.getNombre() +" "+apellido.getApellido());
+       nombrecompleto.accept(alumno1,alumno1);
 
 
-        nombrecompleto.accept(alumno.getNombre(),alumno.getApellido());
-
-        System.out.println(  igualandoCalificaciones.test(8,9));
 
 
     }
